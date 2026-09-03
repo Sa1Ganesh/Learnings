bf86b392dfbf14d5a2536ac283f098a26d0e81af
Sa1Ganesh <saiganesh1w@gmail.com>
2026-09-03 12:05:17 +0530

    Python Basics

--- Py_Baraa/D_sample.py
+++ Py_Baraa/D_sample.py
@@ -1,3 +1,24 @@
 print("Sai Ram Sai Ganesh,Please Don't Give Up")
-
 print("All The Very Best")
+
+#This is a comment
+""" this is comments """
+
+#print
+# print("""Sai""")
+
+#Escape sequences ( \, \\, \n, \t, \b, )
+# print("Hi \"Python\"") 
+# print('Hello "Python"')
+# print("Path: C:\\Users\\Baraa")
+# print("Message1\nMessage2")
+# print("Header\n\t-Sub1\n\t-Sub2")
+print("""Your learning Path
+\tPython Basics
+\tData Engineering
+AI\tML
+""")
+
+
+
+
