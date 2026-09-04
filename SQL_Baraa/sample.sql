42342fe9bad720c848f3224dc6430b1b88ff3ceb
Sa1Ganesh <saiganesh1w@gmail.com>
2026-09-04 12:05:29 +0530

    SQl journey

--- SQL_Baraa/sample.sql
+++ SQL_Baraa/sample.sql
@@ -1,6 +1,17 @@
-SELECT name, create_date 
-FROM sys.databases;
+-- SELECT name, create_date FROM sys.databases;
 
-select * from Sales.Customers;
+-- select * from Sales.Customers;
 
+-- SELECT name FROM sys.databases;
 
+-- use MyDatabase;
+
+-- CREATE TABLE persons (
+-- id int NOT NULL,
+-- person_name VARCHAR(50) NOT NULL,
+-- birth_name DATE,
+-- phone VARCHAR(15) NOT NULL,
+-- CONSTRAINT pk_persons PRIMARY KEY(id)
+-- );
+
+-- SELECT * FROM persons;
