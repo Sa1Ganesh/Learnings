19149096910692845b745c945ac0b18090dc02bc
Sa1Ganesh <saiganesh1w@gmail.com>
2026-09-05 10:57:28 +0530

    Python Basics

--- Py_Baraa/D_sample.py
+++ Py_Baraa/D_sample.py
@@ -1,8 +1,8 @@
-print("Sai Ram Sai Ganesh,Please Don't Give Up")
-print("All The Very Best")
+# print("Sai Ram Sai Ganesh,Please Don't Give Up")
+# print("All The Very Best")
 
 #This is a comment
-""" this is comments """
+""" this is a comment """
 
 #print
 # print("""Sai""")
@@ -13,12 +13,50 @@ print("All The Very Best")
 # print("Path: C:\\Users\\Baraa")
 # print("Message1\nMessage2")
 # print("Header\n\t-Sub1\n\t-Sub2")
-print("""Your learning Path
-\tPython Basics
-\tData Engineering
-AI\tML
-""")
+# print("""Your learning Path
+# \tPython Basics
+# \tData Engineering
+# AI\tML
+# """)
+# print("file","flush",sep=" ",end=" *")  
+
+# Variables
+# print("Sai")
+# print("MY name is Sai")
+# print("Sai wants to learn Python\n")
+
+# name="Ganesh"
+# print(name)
+# print("My name is",name)
+# print(name,"wants to learn Python\n")
+
+# name="Sai Ganesh"
+# print(name)
+# print("My name is",name)
+# print(name,"wants to learn Python")
 
 
+# Input function
+# input("Enter your Name:")
+# name=input("Enter your Name:")
+# country="India"
+# print(name,"is from",country)
+
+# Data Types
+a = 5 #int
+b = "Sai" #str
+c = 3.5 #float
+d = True #bool
+e = "123"
+h = None #None type
+j="" #blank (str)
+k=" " #space (str)
+# print(a,b,c,d,e,h,j,k)
+
+# Functions and Methods
+# print(type("Sai")) #print(value),type(value) functions
+# print("Sai".upper()) #value.upper() methods
+# a=5
+# print(a.bit_length())
 
 
