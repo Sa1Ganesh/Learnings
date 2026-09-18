3d01106daec4215bd0545ba8ff0b8c6348bb7b56
Sa1Ganesh <saiganesh1w@gmail.com>
2026-09-18 12:48:59 +0530

    Gold Layer

--- SQL_Baraa/project.sql
+++ SQL_Baraa/project.sql
@@ -1315,7 +1315,7 @@
 --   cst_create_date
 --   from (
 --   select *, row_number() over (partition by cst_id order by cst_create_date desc) as flag_last 
---   from Silver.crm_cust_info where cst_id is not null
+--   from bronze.crm_cust_info where cst_id is not null
 --   )t where flag_last = 1;
 
 --  set @end_time =getdate();
@@ -1589,3 +1589,240 @@
 -- Load both Layers
 -- exec bronze.load_silver;
 -- exec silver.load_silver;
+
+
+
+-- Gold Layer
+
+-- select * from silver.crm_cust_info;
+
+-- select 
+-- cst_id,
+-- cst_key,
+-- cst_firstname,
+-- cst_lastname,
+-- cst_material_status,
+-- cst_gender,
+-- cst_create_date
+-- from silver.crm_cust_info;
+
+-- for duplicates
+
+-- select cst_id, count(*) from 
+-- (
+-- select 
+-- ci.cst_id,
+-- ci.cst_key,
+-- ci.cst_firstname,
+-- ci.cst_lastname,
+-- ci.cst_material_status,
+-- ci.cst_gender,
+-- ci.cst_create_date,
+-- ca.bdate,
+-- ca.gen,
+-- la.cntry
+-- from silver.crm_cust_info ci
+-- left join silver.erp_cust_az12 ca
+-- on ci.cst_key = ca.cid
+-- left join silver.erp_loc_a101 la
+-- on  ci.cst_key = la.cid
+-- )t 
+-- group by cst_id
+-- having count(*) >1;
+
+
+-- select 
+-- distinct
+-- ci.cst_gender,
+-- ca.gen
+-- from silver.crm_cust_info ci
+-- left join silver.erp_cust_az12 ca
+-- on ci.cst_key = ca.cid
+-- left join silver.erp_loc_a101 la
+-- on  ci.cst_key = la.cid 
+-- order by 1,2;
+
+--Data Integration
+
+-- select 
+-- distinct
+-- ci.cst_gender,
+-- ca.gen,
+-- case 
+--  when ci.cst_gender != 'n/a' then ci.cst_gender --CRM is the master for gender info
+--  else coalesce(ca.gen,'n/a') 
+-- end as new_gender
+-- from silver.crm_cust_info ci
+-- left join silver.erp_cust_az12 ca
+-- on ci.cst_key = ca.cid
+-- left join silver.erp_loc_a101 la
+-- on  ci.cst_key = la.cid 
+-- order by 1,2;
+
+
+-- Readabilty
+
+-- select 
+-- ci.cst_id as customer_id,
+-- ci.cst_key as customer_number,
+-- ci.cst_firstname as first_name,
+-- ci.cst_lastname as last_name,
+-- la.cntry as country,
+-- ci.cst_material_status as marital_status,
+-- case 
+--  when ci.cst_gender != 'n/a' then ci.cst_gender --CRM is the master for gender info
+--  else coalesce(ca.gen,'n/a') 
+-- end as new_gender,
+-- ca.bdate as birth_date,
+-- ci.cst_create_date as create_date
+-- from silver.crm_cust_info ci
+-- left join silver.erp_cust_az12 ca
+-- on ci.cst_key = ca.cid
+-- left join silver.erp_loc_a101 la
+-- on  ci.cst_key = la.cid;
+
+
+
+--Create a View
+
+-- Create View gold.dim_customers as 
+-- select 
+-- row_number () over (order by cst_id) as customer_key,
+-- ci.cst_id as customer_id,
+-- ci.cst_key as customer_number,
+-- ci.cst_firstname as first_name,
+-- ci.cst_lastname as last_name,
+-- la.cntry as country,
+-- ci.cst_material_status as marital_status,
+-- case 
+--  when ci.cst_gender != 'n/a' then ci.cst_gender --CRM is the master for gender info
+--  else coalesce(ca.gen,'n/a') 
+-- end as gender,
+-- ca.bdate as birth_date,
+-- ci.cst_create_date as create_date
+-- from silver.crm_cust_info ci
+-- left join silver.erp_cust_az12 ca
+-- on ci.cst_key = ca.cid
+-- left join silver.erp_loc_a101 la
+-- on  ci.cst_key = la.cid;
+
+
+-- select * from gold.dim_customers;
+-- select distinct gender from gold.dim_customers;
+
+
+
+-- select 
+-- pn.prd_id,
+-- pn.cat_id,
+-- pn.prd_key,
+-- pn.prd_nm,
+-- pn.prd_cost,
+-- pn.prd_line,
+-- pn.prd_start_dt,
+-- pn.prd_end_dt
+-- from silver.crm_prd_info pn
+-- where prd_end_dt is null  ; --Filter out all Historical Data;
+
+-- select 
+-- pn.prd_id,
+-- pn.cat_id,
+-- pn.prd_key,
+-- pn.prd_nm,
+-- pn.prd_cost,
+-- pn.prd_line,
+-- pn.prd_start_dt,
+-- pn.prd_end_dt,
+-- pc.cat,
+-- pc.subcat,
+-- pc.maintenance
+-- from silver.crm_prd_info pn
+-- left join silver.erp_px_cat_g1v2 pc
+-- on pn.cat_id = pc.id 
+-- where prd_end_dt is null ; 
+
+
+
+-- select prd_key, count(*) from 
+-- (
+-- select 
+-- pn.prd_id,
+-- pn.cat_id,
+-- pn.prd_key,
+-- pn.prd_nm,
+-- pn.prd_cost,
+-- pn.prd_line,
+-- pn.prd_start_dt,
+-- pn.prd_end_dt,
+-- pc.cat,
+-- pc.subcat,
+-- pc.maintenance
+-- from silver.crm_prd_info pn
+-- left join silver.erp_px_cat_g1v2 pc
+-- on pn.cat_id = pc.id 
+-- where prd_end_dt is null 
+-- )
+-- t group by prd_key 
+-- having count(*) > 1; 
+
+
+--Create a View
+
+-- create view gold.dim_products as 
+-- select 
+-- row_number() over (order by pn.prd_start_dt, pn.prd_key) as product_key,
+-- pn.prd_id as product_id,
+-- pn.prd_key as product_number,
+-- pn.prd_nm as product_name,
+-- pn.cat_id as category_id ,
+-- pc.cat as category,
+-- pc.subcat as subcategory,
+-- pc.maintenance ,
+-- pn.prd_cost as cost,
+-- pn.prd_line as product_line,
+-- pn.prd_start_dt as start_date
+-- from silver.crm_prd_info pn
+-- left join silver.erp_px_cat_g1v2 pc
+-- on pn.cat_id = pc.id 
+-- where prd_end_dt is null 
+
+
+-- select * from gold.dim_products;
+
+
+
+-- Create a View  
+
+-- create view gold.fact_sales as
+-- select 
+-- sd.sls_ord_num as order_number,
+-- pr.product_key,
+-- cu.customer_key,
+-- sd.sls_order_dt as order_date,
+-- sd.sls_ship_dt as shipping_date,
+-- sd.sls_due_dt as due_date,
+-- sd.sls_sales as sales_amount,
+-- sd.sls_quantity as quantity,
+-- sd.sls_price as price
+-- from silver.crm_sales_details sd 
+-- left join gold.dim_products pr
+-- on sd.sls_prd_key = pr.product_number
+-- left join gold.dim_customers cu
+-- on sd.sls_cust_id = cu.customer_id;
+
+-- select * from gold.fact_sales;
+
+-- Foreign Key Integrity (Dimensions)
+-- select * 
+-- from gold.fact_sales f
+-- left join gold.dim_customers c
+-- on c.customer_key = f.customer_key
+-- where c.customer_key is null;
+
+-- select * 
+-- from gold.fact_sales f
+-- left join gold.dim_customers c
+-- on c.customer_key = f.customer_key
+-- left join gold.dim_products p
+-- on p.product_key = f.product_key 
+-- where p.product_key is null;
